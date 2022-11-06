ad1058e899c1fd0f81e673d1b1eb38d3e75c28a3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-06 15:38:33 -0800

    Fix backup scripts for real

--- images/universalis-s3sync-mariadb/dump-database.sh
+++ images/universalis-s3sync-mariadb/dump-database.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-rm $DUMP_PATH
-exec mysqldump -h"$REMOTE_MYSQL_ADDR" -P"$REMOTE_MYSQL_PORT" -uroot -p"$REMOTE_MYSQL_ROOT_PASSWORD" $REMOTE_MYSQL_DATABASE > $DUMP_PATH
+#!/bin/bash
+rm -f $DUMP_PATH
+echo Dumping database...
+mysqldump -h"$REMOTE_MYSQL_ADDR" -P"$REMOTE_MYSQL_PORT" -uroot -p"$REMOTE_MYSQL_ROOT_PASSWORD" $REMOTE_MYSQL_DATABASE > $DUMP_PATH
+echo Database dumped. Beginning sync...
+/app/sync.sh
+echo Sync complete.
--- images/universalis-s3sync/run.sh
+++ images/universalis-s3sync/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws configure import --csv file://$AWS_SECRET_CSV
-aws s3 sync $SYNC_SOURCE $SYNC_TARGET --endpoint-url $AWS_ENDPOINT --profile $AWS_PROFILE --dryrun
+aws s3 sync $SYNC_SOURCE $SYNC_TARGET --endpoint-url $AWS_ENDPOINT --profile $AWS_PROFILE
