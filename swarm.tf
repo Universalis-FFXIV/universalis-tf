93a609996965b85d01704d4ac0f3b591b1542163
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-13 08:36:51 -0800

    Upgrade postgres node

--- swarm.tf
+++ swarm.tf
@@ -179,7 +179,7 @@ resource "hcloud_server" "swarm_worker_1" {
 # Postgres is assigned to this node
 resource "hcloud_server" "swarm_worker_2" {
   name               = "swarm-worker-2"
-  server_type        = "cpx31"
+  server_type        = "cx41"
   image              = "docker-ce"
   location           = "hel1"
   keep_disk          = true
