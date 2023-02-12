7007d92ed83e905ef3df8cf1212f7936c5d54c6c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-02-12 15:16:55 -0800

    Update infra diagram

--- viz/diagram.py
+++ viz/diagram.py
@@ -1,87 +1,110 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import Lambda
 from diagrams.aws.network import APIGateway
 from diagrams.generic.storage import Storage
-from diagrams.onprem.certificates import LetsEncrypt
 from diagrams.onprem.compute import Server
 from diagrams.onprem.container import Docker
-from diagrams.onprem.database import Mariadb, Postgresql
+from diagrams.onprem.database import Mariadb, Postgresql, Scylla
 from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.monitoring import Grafana
 from diagrams.onprem.network import Nginx, Traefik
 from diagrams.onprem.queue import RabbitMQ
-from diagrams.programming.language import Csharp
-from third_party import Swarmpit, SwarmCronjob, Scylla, Cloudflare, Nextjs, Victoria, Tempo, OpenTelemetry
+from diagrams.programming.language import Csharp, Rust
+from third_party import (
+    Discord,
+    Swarmpit,
+    SwarmCronjob,
+    Cloudflare,
+    Nextjs,
+    Victoria,
+    Tempo,
+    OpenTelemetry,
+)
 
 with Diagram("Universalis", show=False):
     with Cluster("Lodestone API"):
         lodestone_apig = APIGateway("lodestone.universalis.app")
         search_characters = Lambda("search_characters")
         get_character = Lambda("get_character")
-        lodestone_apig >> search_characters  # type: ignore
-        lodestone_apig >> get_character  # type: ignore
+        lodestone_apig >> search_characters
+        lodestone_apig >> get_character
 
-    with Cluster("Universalis Redis (Dedicated Server)"):
+    with Cluster("Universalis Database (Dedicated Server)"):
         universalis_redis = Redis("Stats & Tax Rates")
-        universalis_redis - Redis("Replica")  # type: ignore
+        universalis_redis - Redis("Replica")
+        universalis_listings_db = Postgresql()
+
+    with Cluster("Universalis Sales Database"):
+        universalis_sales_db = Scylla()
+        universalis_sales_db - Scylla() - Scylla()
+
+    discord = Discord("Discord")
 
     cloudflare = Cloudflare("DNS")
 
     swarm = Docker("Docker Swarm")
-    cloudflare >> lodestone_apig  # type: ignore
-    cloudflare >> Server("Cloud Load Balancer") >> swarm  # type: ignore
+    cloudflare >> lodestone_apig
+    cloudflare >> Server("Cloud Load Balancer") >> swarm
 
     with Cluster("Cluster Services"):
         with Cluster("Swarm Infra"):
             ingress = Traefik("Traefik")
             swarm_cronjob = SwarmCronjob("swarm-cronjob")
-            ca = LetsEncrypt("CA")
-            ingress >> ca  # type: ignore
-            swarm >> ingress  # type: ignore
-        
+            swarmpit = Swarmpit("Swarmpit")
+            swarm >> ingress >> swarmpit
+
         with Cluster("Monitoring Stack"):
+            metrics_edge = Edge(label="metrics", color="darkorange")
+
             grafana = Grafana("Grafana")
             tempo = Tempo("Tempo")
-            otel = OpenTelemetry("OpenTelemetry")
             victoria = Victoria("Victoria")
-            
-            swarmpit = Swarmpit("Swarmpit")
 
-            ingress >> grafana >> victoria  # type: ignore
-            grafana >> tempo  # type: ignore
-            victoria >> tempo  # type: ignore
-            tempo >> otel  # type: ignore
-            ingress >> swarmpit  # type: ignore
+            (
+                victoria
+                >> metrics_edge
+                >> tempo
+                >> Edge(label="traces", color="blue")
+                >> OpenTelemetry("OpenTelemetry")
+            )
+            ingress >> grafana >> metrics_edge >> [victoria, tempo]
 
         with Cluster("Universalis"):
             universalis_router = Nginx("universalis.app")
 
             with Cluster("Universalis API"):
                 universalis_api = Csharp("API")
-
                 universalis_mq = RabbitMQ("WebSocket Events")
-                universalis_sales_db = Scylla("Sales Database")
-                universalis_listings_db = Postgresql("Listings Database")
+                universalis_cache = Redis("Cache")
 
-                with Cluster("Distributed Cache"):
-                    universalis_cache = Redis("Redis")
-
-                universalis_api >> universalis_mq  # type: ignore
-                universalis_api >> universalis_redis  # type: ignore
-                universalis_api >> universalis_sales_db  # type: ignore
-                universalis_api >> universalis_listings_db  # type: ignore
-                universalis_api >> universalis_cache  # type: ignore
-                universalis_router >> universalis_api  # type: ignore
-
-                ingress >> universalis_router  # type: ignore
+                universalis_api >> Edge(forward=True, reverse=True) << universalis_mq
+                universalis_api >> universalis_sales_db
+                (
+                    ingress
+                    >> universalis_router
+                    >> universalis_api
+                    >> [
+                        universalis_redis,
+                        universalis_listings_db,
+                        universalis_cache,
+                    ]
+                )
 
             with Cluster("Mogboard"):
                 mogboard_website = Nextjs("Website")
                 mogboard_db = Mariadb("Website Database")
-                universalis_router >> mogboard_website >> mogboard_db  # type: ignore
+                mogboard_website >> universalis_api
+                universalis_alerts = Rust("Universalis Alerts")
+                universalis_alerts >> [universalis_api, mogboard_db]
+                universalis_alerts >> Edge(label="webhook", style="dashed") >> discord
+                universalis_router >> mogboard_website >> mogboard_db
 
         with Cluster("Universalis Documentation"):
-            ingress >> Nextjs("docs.universalis.app")  # type: ignore
+            ingress >> Nextjs("docs.universalis.app")
 
         with Cluster("ACT Plugin Resources"):
-            ingress >> Nginx("act.universalis.app") >> Storage("version\ndefinitions.json")  # type: ignore
+            (
+                ingress
+                >> Nginx("act.universalis.app")
+                >> Storage("version\ndefinitions.json")
+            )
--- viz/third_party.py
+++ viz/third_party.py
@@ -1,10 +1,18 @@
 from urllib.request import urlretrieve
 from diagrams.custom import Custom
 
+
+class Discord(Custom):
+    def __init__(self, label):
+        discord_icon = "resources/discord.png"
+        super().__init__(label, discord_icon)
+
+
 class Cloudflare(Custom):
     def __init__(self, label):
         cloudflare_icon = "resources/cf.png"
         super().__init__(label, cloudflare_icon)
+
 
 class Nextjs(Custom):
     def __init__(self, label):
@@ -13,10 +21,6 @@ class Nextjs(Custom):
         urlretrieve(nextjs_icon_url, nextjs_icon)
         super().__init__(label, nextjs_icon)
 
-class Scylla(Custom):
-    def __init__(self, label):
-        scylla_icon = "resources/scylla-opensource.png"
-        super().__init__(label, scylla_icon)
 
 class SwarmCronjob(Custom):
     def __init__(self, label):
@@ -25,12 +29,14 @@ class SwarmCronjob(Custom):
         urlretrieve(swarm_cronjob_url, swarm_cronjob_icon)
         super().__init__(label, swarm_cronjob_icon)
 
+
 class Swarmpit(Custom):
     def __init__(self, label):
         swarmpit_url = "https://raw.githubusercontent.com/swarmpit/swarmpit/master/resources/public/img/icon.png"
         swarmpit_icon = "resources/swarmpit.png"
         urlretrieve(swarmpit_url, swarmpit_icon)
         super().__init__(label, swarmpit_icon)
+
 
 class Victoria(Custom):
     def __init__(self, label):
@@ -39,10 +45,12 @@ class Victoria(Custom):
         urlretrieve(victoria_url, victoria_icon)
         super().__init__(label, victoria_icon)
 
+
 class Tempo(Custom):
     def __init__(self, label):
         tempo_icon = "resources/grafana-tempo.png"
         super().__init__(label, tempo_icon)
+
 
 class OpenTelemetry(Custom):
     def __init__(self, label):
