e1565d2d28927c25b7f457a1564df3b311ee98e6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-07-19 15:38:54 -0700

    Implement reindexer script

---
+++ images/universalis-reindexer/reindexer.py
@@ -0,0 +1,17 @@
+import logging
+import psycopg2
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+logging.info("Starting reindexing script.")
+
+with psycopg2.connect("host=10.0.200.3 dbname=universalis user=universalis password=universalis") as pgsql_conn:
+    logging.info("Connected to PostgreSQL database.")
+
+    with pgsql_conn.cursor(name="universalis-reindexer") as pgsql_cur:
+        logging.info("Opened database cursor \"universalis-reindexer\".")
+
+        pgsql_cur.execute("REINDEX INDEX CONCURRENTLY \"IX_listing_all_item_id_world_id\"");
+        pgsql_cur.execute("REINDEX INDEX CONCURRENTLY \"ix_market_item_all_item_id_world_id\"");
+
+        logging.info("Completed reindexing job.")
